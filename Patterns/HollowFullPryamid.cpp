6f10b7d2eb992baa4ac0f2af8f6d35a46328182a
Deepak Vaprani <deepakvaprani03@gmail.com>
2023-08-21 13:14:37 +0530

    Patterns

---
+++ Patterns/HollowFullPryamid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int size;
+  cout<<"Please enter the size of the hollow full pryamid"<<endl;
+  cin>>size;
+
+
+  for(int i=0;i<size;i++){
+  
+  for(int j=0;j<size-1;j++){
+      if(i == size - j-1 || i==size-1) cout<<"* ";
+      else cout<<"  ";
+    }
+
+    for(int j=0;j<size;j++){
+      if(i == j||i==size-1) cout<<"* ";
+      else cout<<"  ";
+    }
+    cout<<endl;
+  }
+  return 0;
+}
