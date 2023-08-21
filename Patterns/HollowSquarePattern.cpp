9ad84c071e25ed666846776d8c0e0118c88d58ac
Deepak Vaprani <deepakvaprani03@gmail.com>
2023-08-21 12:44:00 +0530

    Patterns

--- Patterns/HollowSquarePattern.cpp
+++ Patterns/HollowSquarePattern.cpp
@@ -3,16 +3,15 @@ using namespace std;
 
 int main() {
   int size;
-  cout<<"Please enter the size of the square"<<endl;
+  cout<<"Please enter the size of the hollow square"<<endl;
   cin>>size;
 
   for(int i=0;i<size;i++){
-    cout<<"* ";
-    for(int j=0;j<size-2;j++){
-      if(i == 0 || i == size-1) cout<<"* ";
+    for(int j=0;j<size;j++){
+      if(i == 0 || i == size-1 || j==0 || j== size-1) cout<<"* ";
       else cout<<"  ";
     }
-    cout<<"* "<<endl;
+    cout<<endl;
   }
   return 0;
 }
