98f14add8bae1bd46f2d147b6c6ea7b21c1b6c42
Deepak Vaprani <deepakvaprani03@gmail.com>
2023-08-28 10:55:01 +0530

    BasicQuestions

---
+++ BasicQuestions/CircleRadius.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  float radius;
+
+  cout<<"Enter the radius of the circle "<<endl;
+  cin>>radius;
+
+  cout<<"Area of the circle is "<<3.14*radius*radius<<endl;
+  
+  return 0;
+}
