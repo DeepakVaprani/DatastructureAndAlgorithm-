a19140c495c1ba2c5ba192273cfb95b16eb5899d
Deepak Vaprani <deepakvaprani03@gmail.com>
2023-09-02 11:59:34 +0530

    Array

---
+++ Arrays/IntersectingElement.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  vector<int>v; 
+  vector<int>v1;
+
+  int size, size1,a;
+
+  cout<<"Enter size of array1"<<endl;
+  cin>>size;
+
+  cout<<"Enter the element of array 1"<<endl;
+
+  for(int i=0;i<size;i++){
+    cin>>a;
+    v.push_back(a);
+  }
+  cout<<"Enter size of array2"<<endl;
+  cin>>size1;
+  
+  cout<<"Enter the element of array 2"<<endl;
+    for(int i=0;i<size1;i++){
+    cin>>a;
+    v1.push_back(a);
+  }
+
+  for(int i =0;i<size;i++){
+    for(int j=0;j<size1;j++){
+      if(v[i]==v1[j]) {
+        cout<<"Intersecting element is "<<v[i]<<endl;
+        break;
+      }
+    }
+  }
+
+
+  return 0;
+}
